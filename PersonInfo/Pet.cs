316918d19baeaaa868f3563e9c2d90e7d7b70e2f
Kriskooo <kris.danielov@gmail.com>
2021-03-11 15:23:23 +0200

    InterfacesAndAbstractionExercise

---
+++ PersonInfo/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Pet : IPet
+    {
+        public Pet(string name, string birthdate)
+        {
+            this.Name = name;
+            this.Birthdate = birthdate;
+        }
+        public string Name { get; set; }
+        public string Birthdate { get; set; }
+        public override string ToString() => $"{this.Birthdate}";
+    }
+}
---
+++ Telephony/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = ReadConsole();
+            string[] url = ReadConsole();
+            ICall calling = null;
+            foreach (var item in numbers)
+            {
+                if (item.Length == 10)
+                {
+                    calling = new Smartphone();
+                    calling.Calling(item);
+                }
+                else
+                {
+                    calling = new StationaryPhone();
+                    calling.Calling(item);
+                }
+            }
+            foreach (var item in url)
+            {
+                IBrowse browse = new Smartphone();
+                browse.Browsing(item);
+            }
+        }
+        private static string[] ReadConsole() => Console.ReadLine()
+            .Split();
+    }
+    }
+}
